cbd66471885be4d24976a636af88e59cd175b91d
Todd Ayers <taayers712@gmail.com>
2021-11-22 17:15:37 -0500

    GussThatFlag Project

---
+++ GuessThatFlag/GuessThatFlag/GuessThatFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThatFlagApp.swift
+//  GuessThatFlag
+//
+//  Created by Todd Ayers on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessThatFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
