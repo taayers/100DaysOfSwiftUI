3844303fa495fb0e351b24b26f0529a88a1122e2
Todd Ayers <taayers712@gmail.com>
2021-11-24 12:18:47 -0500

    WordScramble Project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Todd Ayers on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
