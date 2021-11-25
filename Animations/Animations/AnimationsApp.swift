e9261543520bf28cfea229b94397265b3159ed32
Todd Ayers <taayers712@gmail.com>
2021-11-25 13:09:41 -0500

    Animation Project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Todd Ayers on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
