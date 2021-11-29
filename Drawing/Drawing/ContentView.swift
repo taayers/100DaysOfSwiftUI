143020e45cd641a67055447d1abd442c81b53cf4
Todd Ayers <taayers712@gmail.com>
2021-11-29 14:53:23 -0500

    Drawing Project

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Todd Ayers on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+
+        var body: some View {
+            
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Todd Ayers on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
